26c6d0ff019f908bc3cbaa247a9590d2b7a41c3c
Anthony-25 <faustoantonio0325@gmail.com>
2026-02-02 21:38:56 -0400

    ejercicio 4

--- ejercicio 4/ejercicio 4/Form1.Designer.cs
+++ ejercicio 4/ejercicio 4/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            ingrese = new Label();
+            txtNum1 = new TextBox();
+            txtNum2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtRes = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(126, 190);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "=";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // ingrese
+            // 
+            ingrese.AutoSize = true;
+            ingrese.Location = new Point(77, 65);
+            ingrese.Name = "ingrese";
+            ingrese.Size = new Size(93, 15);
+            ingrese.TabIndex = 1;
+            ingrese.Text = "Ingrese  numero";
+            ingrese.Click += ingrese_Click;
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(185, 57);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 2;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(185, 102);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(77, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Ingrese  numero";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(91, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Resultado:";
+            // 
+            // txtRes
+            // 
+            txtRes.Location = new Point(185, 147);
+            txtRes.Name = "txtRes";
+            txtRes.Size = new Size(100, 23);
+            txtRes.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(615, 314);
+            Controls.Add(txtRes);
+            Controls.Add(label2);
+            Controls.Add(txtNum2);
+            Controls.Add(label1);
+            Controls.Add(txtNum1);
+            Controls.Add(ingrese);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label ingrese;
+        private TextBox txtNum1;
+        private TextBox txtNum2;
+        private Label label1;
+        private Label label2;
+        private TextBox txtRes;
     }
 }
--- ejercicio 4/ejercicio 4/Form1.cs
+++ ejercicio 4/ejercicio 4/Form1.cs
@@ -6,5 +6,25 @@ namespace ejercicio_4
         {
             InitializeComponent();
         }
+
+        private void ingrese_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            a = int.Parse(txtNum1.Text);
+            b = int.Parse(txtNum2.Text);
+            res = a + b;
+            txtRes.Text = res.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
