4ab3afca8c63af2685f53911f992148f5ce2ab7d
Anthony-25 <faustoantonio0325@gmail.com>
2026-02-05 21:33:35 -0400

    ejercicio 10

--- ejercicio 10/ejercicio 10/Form1.Designer.cs
+++ ejercicio 10/ejercicio 10/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ejercicio_9
+﻿namespace ejercicio_10
 {
     partial class Form1
     {
@@ -28,35 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            textBox1 = new TextBox();
             button1 = new Button();
             SuspendLayout();
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(36, 91);
-            label1.Name = "label1";
-            label1.Size = new Size(49, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Ingresar";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(131, 83);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
-            // 
             // button1
             // 
-            button1.Location = new Point(272, 83);
+            button1.Location = new Point(211, 83);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 2;
-            button1.Text = "Validar";
+            button1.Size = new Size(321, 172);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -64,20 +45,16 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(486, 271);
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(730, 380);
             Controls.Add(button1);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private TextBox textBox1;
         private Button button1;
     }
 }
