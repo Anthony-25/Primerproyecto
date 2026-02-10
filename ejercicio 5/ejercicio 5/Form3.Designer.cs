bf423dd0fe5ca1214f8cb2a61bf123cf62c73406
Anthony-25 <faustoantonio0325@gmail.com>
2026-02-09 23:46:59 -0400

    ejercicio 5

---
+++ ejercicio 5/ejercicio 5/Form3.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ejercicio_5
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rdbOpcion1 = new RadioButton();
+            rdbOpcion2 = new RadioButton();
+            rdbOpcion3 = new RadioButton();
+            btnAceptar = new Button();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // rdbOpcion1
+            // 
+            rdbOpcion1.AutoSize = true;
+            rdbOpcion1.Location = new Point(146, 89);
+            rdbOpcion1.Name = "rdbOpcion1";
+            rdbOpcion1.Size = new Size(73, 19);
+            rdbOpcion1.TabIndex = 0;
+            rdbOpcion1.TabStop = true;
+            rdbOpcion1.Text = "Opcion 1";
+            rdbOpcion1.UseVisualStyleBackColor = true;
+            // 
+            // rdbOpcion2
+            // 
+            rdbOpcion2.AutoSize = true;
+            rdbOpcion2.Location = new Point(224, 128);
+            rdbOpcion2.Name = "rdbOpcion2";
+            rdbOpcion2.Size = new Size(73, 19);
+            rdbOpcion2.TabIndex = 1;
+            rdbOpcion2.TabStop = true;
+            rdbOpcion2.Text = "Opcion 2";
+            rdbOpcion2.UseVisualStyleBackColor = true;
+            // 
+            // rdbOpcion3
+            // 
+            rdbOpcion3.AutoSize = true;
+            rdbOpcion3.Location = new Point(338, 159);
+            rdbOpcion3.Name = "rdbOpcion3";
+            rdbOpcion3.Size = new Size(73, 19);
+            rdbOpcion3.TabIndex = 2;
+            rdbOpcion3.TabStop = true;
+            rdbOpcion3.Text = "Opcion 3";
+            rdbOpcion3.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Location = new Point(382, 95);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.TabIndex = 3;
+            btnAceptar.Text = "Agregar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += BtnAceptar_Click;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(266, 46);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(0, 15);
+            lblResultado.TabIndex = 4;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(btnAceptar);
+            Controls.Add(rdbOpcion3);
+            Controls.Add(rdbOpcion2);
+            Controls.Add(rdbOpcion1);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RadioButton rdbOpcion1;
+        private RadioButton rdbOpcion2;
+        private RadioButton rdbOpcion3;
+        private Button btnAceptar;
+        private Label lblResultado;
+    }
+}
---
+++ ejercicio 5/ejercicio 5/Form3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ejercicio_5
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            if (rdbOpcion1.Checked)
+                lblResultado.Text = "Opcion 1 seleccionada";
+            else if (rdbOpcion2.Checked)
+                lblResultado.Text = "Opcion 2 seleccionada";
+            else if (rdbOpcion3.Checked)
+                lblResultado.Text = "Opcion 3 seleccionada";
+            else
+                lblResultado.Text = "No seleccionaste ninguna opcion";
+
+        }
+    }
+}
