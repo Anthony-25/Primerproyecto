cb941e3c56f3ddff65c2fd9bcaab1ab5d84c0ded
Anthony-25 <faustoantonio0325@gmail.com>
2026-02-09 23:04:19 -0400

    ejercicio 7

--- ejercicio 7/ejercicio 7/Form1.Designer.cs
+++ ejercicio 7/ejercicio 7/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            txtElemento = new TextBox();
+            lstElementos = new ListBox();
+            btnAgregar = new Button();
+            SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            txtElemento.Location = new Point(266, 136);
+            txtElemento.Name = "txtElemento";
+            txtElemento.Size = new Size(100, 23);
+            txtElemento.TabIndex = 0;
+            // 
+            // lstElementos
+            // 
+            lstElementos.FormattingEnabled = true;
+            lstElementos.Location = new Point(266, 165);
+            lstElementos.Name = "lstElementos";
+            lstElementos.Size = new Size(120, 94);
+            lstElementos.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(392, 200);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(75, 23);
+            btnAgregar.TabIndex = 2;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnAgregar);
+            Controls.Add(lstElementos);
+            Controls.Add(txtElemento);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtElemento;
+        private ListBox lstElementos;
+        private Button btnAgregar;
     }
 }
--- ejercicio 7/ejercicio 7/Form1.cs
+++ ejercicio 7/ejercicio 7/Form1.cs
@@ -6,5 +6,24 @@ namespace ejercicio_7
         {
             InitializeComponent();
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtElemento.Text))
+            {
+                lstElementos.Items.Add(txtElemento.Text);
+                txtElemento.Clear();
+                txtElemento.Focus();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un texto antes de agregar");
+
+            }
+
+
+        }
     }
 }
